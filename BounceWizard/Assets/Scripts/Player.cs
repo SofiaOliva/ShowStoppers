cd876200041582c838fc23a34a8de302938b86c6
LordTepig <tyler8555@gmail.com>
2022-09-25 13:55:57 -0400

    Fireballs and Aiming

--- BounceWizard/Assets/Scripts/Player.cs
+++ BounceWizard/Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : Entity
 {
+    public GameObject fireball;
+    public float fireBallStartDistance = 1.25f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +24,11 @@ public class Player : Entity
 
     void LaunchFireball()
     {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPosition = new Vector3(worldPosition.x, 0, worldPosition.z);
+        Vector3 aimDirection = (worldPosition - transform.position).normalized;
+        GameObject newFireball = Instantiate(fireball, transform.position + aimDirection*fireBallStartDistance, Quaternion.identity);
+        newFireball.GetComponent<Rigidbody>().velocity = aimDirection;
         print("Launching fireball");
         //Tyler: Make a fireball prefab, and use Instantiate() here to create it in the scene
     }
--- BounceWizard/Assets/Scripts/SpeedController.cs
+++ BounceWizard/Assets/Scripts/SpeedController.cs
@@ -18,25 +18,25 @@ public class SpeedController : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.velocity = new Vector3(1f, 0f, 1f);
+        rb.velocity = rb.velocity.normalized * speed;
     }
 
     private void FixedUpdate()
     {
         if(rb.velocity.sqrMagnitude == 0f)
         {
-            rb.velocity = Vector3.up;
+            rb.velocity = Vector3.forward;
         }
         float currentSpeed = rb.velocity.magnitude;
         Vector3 currentDirection = rb.velocity.normalized;
 
         if(currentSpeed < speed)
         {
-            rb.velocity = currentDirection * Mathf.Max(currentSpeed + acceleration, speed);
+            rb.velocity = currentDirection * Mathf.Max(currentSpeed + acceleration*Time.fixedDeltaTime, speed);
         }
         else
         {
-            rb.velocity = currentDirection * Mathf.Max(currentSpeed - deacceleration, speed);
+            rb.velocity = currentDirection * Mathf.Max(currentSpeed - deacceleration*Time.fixedDeltaTime, speed);
         }
     }
 }

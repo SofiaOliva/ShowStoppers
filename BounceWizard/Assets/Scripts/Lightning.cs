4af78a2cf49c3dcb8a440839c10a81f94853ca92
Colin Page <colinjpage20@gmail.com>
2022-09-25 16:26:58 -0400

    Lightning

---
+++ BounceWizard/Assets/Scripts/Lightning.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour
+{
+    public Transform enemyRoot;
+
+    public GameObject lightningEffect;
+    ManaPool manaPool;
+
+    public void Awake()
+    {
+        manaPool = FindObjectOfType<ManaPool>();
+    }
+
+    private void OnEnable()
+    {
+        foreach(ManaStream stream in manaPool.manaStreams)
+        {
+            stream.Explode += Strike;
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (ManaStream stream in manaPool.manaStreams)
+        {
+            stream.Explode -= Strike;
+        }
+    }
+
+    private void Strike()
+    {
+        int enemyCount = enemyRoot.childCount;
+        Entity struckEntity = enemyRoot.GetChild(Random.Range(0, enemyCount)).gameObject.GetComponent<Entity>();
+        GameObject spawnedBolt = Instantiate(lightningEffect, struckEntity.transform.position, Quaternion.identity);
+        Destroy(spawnedBolt, 0.25f);
+        struckEntity.Die();
+    }
+}

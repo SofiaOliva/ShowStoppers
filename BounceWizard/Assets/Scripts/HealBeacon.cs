b71a233cef528638fc2dedbaec4eea23e3fb039d
SofiaOliva <89924177+SofiaOliva@users.noreply.github.com>
2022-09-27 20:34:15 -0400

    healbeacon
     skeleton healing

---
+++ BounceWizard/Assets/Scripts/HealBeacon.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealBeacon : MonoBehaviour
+{
+    public int healAmt = 1; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Start with the current health..?
+        startHealth = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Do we update anything?
+    }
+
+    public Heal(){
+        // Start with current health?
+        // Attach healing to beacon's area
+        // After it heals 1, it will be destroyed
+        // Unless we want it to heal for a certain # of seconds
+
+        
+    }
+
+}
